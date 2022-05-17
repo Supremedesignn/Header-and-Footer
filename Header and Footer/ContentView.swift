1c10766927a81256f31c367277d0afd243906661
Nkosi Yafeu <95482668+Supremedesignn@users.noreply.github.com>
2022-05-16 22:10:35 -0400

    Initial Commit

---
+++ Header and Footer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Header and Footer
+//
+//  Created by Nkosi Yafeu on 5/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Header and Footer/Header_and_FooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Header_and_FooterApp.swift
+//  Header and Footer
+//
+//  Created by Nkosi Yafeu on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Header_and_FooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
