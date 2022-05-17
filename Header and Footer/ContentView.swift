1479ccd9ba60f05240f1e6fbd8386be6ba71f2e4
Nkosi Yafeu <95482668+Supremedesignn@users.noreply.github.com>
2022-05-16 22:30:29 -0400

    #How to group a SwiftUI list into a section
    Adding  a section is as easy ad adding data to list. You just need to wrap your data inside a Section view.

--- Header and Footer/ContentView.swift
+++ Header and Footer/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    Text("About")
+                    Text("Software Update")
+                }
+                Section {
+                    Text("AirDrop")
+                    Text("AirPlay & Handoff")
+                    Text("Picture in Picture")
+                    Text("CarPlay")
+                }
+                Section {
+                    Text("iPhone Storage")
+                    Text("Background App Refresh")
+                }
+                Section {
+                    Text("Date & Time")
+                    Text("Keyboard")
+                    Text("Fonts")
+                    Text("Language & Region")
+                }
+            }
+            .navigationTitle("General")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
